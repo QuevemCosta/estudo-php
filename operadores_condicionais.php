f908ad4221ee6674ea63220e0562ff49b6b7a3a1
QuevemCosta <quevemcostadacruz@gmail.com>
2018-08-23 14:14:14 -0300

    ---

---
+++ operadores_condicionais.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Operadores condicionais php</title>
+	</head>
+	<body>
+		<?php
+			
+			$i = 7;
+
+
+			if ($i < 5) {
+				echo 'verdadeiro';
+			}else{
+				echo 'false';
+			}
+		?>
+	</body>
+</html>
